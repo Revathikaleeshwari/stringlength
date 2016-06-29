772268fb0ad8732cd1f701970d5b6627b690378c
Revathikaleeshwari <nrevathibe38@gmail.com>
2016-06-29 11:15:13 +0530

    Create stringlength.c

---
+++ stringlength.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+int length;
+printf("Enter a string to calculate it's length\n");
+scanf(a);
+length=strlen(a);
+printf("Length of entered string is=%d\n",length);
+return 0;
+}
